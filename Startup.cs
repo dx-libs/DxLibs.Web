88be16d6a9a0051a5fa269258e957ccc280087d6
dx Team <32604377+dx-team@users.noreply.github.com>
2018-04-02 03:06:06 -0300

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+#if FW
+using Microsoft.Owin;
+using Owin;
+#elif NETCORE 
+using Microsoft.AspNetCore.Builder;
+//using Microsoft.Extensions.Logging;
+#endif
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DxLibs.Web
+{
+    public abstract class Startup : IStartup
+    {
+#if FW
+        public virtual void Configure(IAppBuilder app)
+#elif NETCORE
+        public virtual void Configure(IApplicationBuilder app)
+#endif
+        {
+            // Para obtener más información sobre cómo configurar la aplicación, visite https://go.microsoft.com/fwlink/?LinkID=316888
+            //RegisterServices();
+        }
+
+#if FW
+        public virtual void Configuration(IAppBuilder app)
+        {
+            Configure(app);
+        }
+#endif
+
+        protected virtual void RegisterServices(IServiceCollection services)
+        {
+        }
+
+        public virtual IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            RegisterServices(services);
+
+            return BuildServiceProvider(services);
+        }
+
+        protected abstract IServiceProvider BuildServiceProvider(IServiceCollection services);
+    }
+}
